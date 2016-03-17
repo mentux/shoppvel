56db5106565d7f76c3cec0e62c72d869acbc7b04
Ademir Mazer Jr <ademir.mazer.jr@gmail.com>
2016-03-17 19:49:58 -0300

    Call ProdutosSeeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
         // $this->call(UsersTableSeeder::class);
 		$this->call(CategoriasSeeder::class);
 		$this->call(MarcasSeeder::class);
+		$this->call(ProdutosSeeder::class);
 		//Model::reguard();
     }
 }
