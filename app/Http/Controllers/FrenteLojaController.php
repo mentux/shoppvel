d5bace12bfcd42945dc0385ee27f394858df89db
Ademir Mazer Jr <ademir.mazer.jr@gmail.com>
2016-03-17 20:53:03 -0300

    Conteudo dinâmico

--- app/Http/Controllers/FrenteLojaController.php
+++ app/Http/Controllers/FrenteLojaController.php
@@ -3,13 +3,14 @@
 namespace Shoppvel\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use Shoppvel\Http\Requests;
 
-class FrenteLojaController extends Controller
-{
+class FrenteLojaController extends Controller {
+
     function getIndex() {
-		$models['categorias'] = \Shoppvel\Models\Categoria::all();
-		return view('layouts.frente-loja', $models);
-	}
+        $models['categorias'] = \Shoppvel\Models\Categoria::all();
+        $models['produtos'] = \Shoppvel\Models\Produto::destacado()->get();
+        return view('frente.entrada', $models);
+    }
+
 }
