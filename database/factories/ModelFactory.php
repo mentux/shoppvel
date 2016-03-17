5ca60750296fb9438188ab5b2fb85852094fd01f
Ademir Mazer Jr <ademir.mazer.jr@gmail.com>
2016-03-17 20:08:31 -0300

    Categoria gerando pai corretamente

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -28,8 +28,10 @@ $factory->define(Shoppvel\Models\Categoria::class, function (Faker\Generator $fa
     return [
         'nome' => $faker->unique()->word,
         'categoria_id' => function () use ($faker) {
-            $pai = Shoppvel\Models\Categoria::find($faker->optional()->numberBetween(1, 20));
-            return $pai;
+            $cats = Shoppvel\Models\Categoria::all('id')->toArray();
+            $cats = array_column($cats, 'id');
+            $pai = Shoppvel\Models\Categoria::find($faker->optional()->randomElement($cats));
+            return $pai == null ? null : $pai->id;
         },
     ];
 });
@@ -49,9 +51,9 @@ $factory->define(Shoppvel\Models\Produto::class, function (Faker\Generator $fake
         'nome' => $faker->text(50),
         'descricao' => $faker->paragraphs,
         'avaliacao_qtde' => $faker->randomNumber(3),
-        'avaliacao_total' => $faker->numberBetween(0,5),
-        'qtde_estoque' => $faker->numberBetween(0,50),
-        'preco_venda' => $faker->randomFloat(2,10,5000),
+        'avaliacao_total' => $faker->numberBetween(0, 5),
+        'qtde_estoque' => $faker->numberBetween(0, 50),
+        'preco_venda' => $faker->randomFloat(2, 10, 5000),
         'destacado' => $faker->boolean(),
         'marca_id' => function () use ($faker) {
             $marca = Shoppvel\Models\Marca::find($faker->optional()->numberBetween(1, 20));
