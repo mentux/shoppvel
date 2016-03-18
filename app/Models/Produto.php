b996fd11a4fb39c6cd4cda4bacce71306f022e55
Ademir Mazer Jr <ademir.mazer.jr@gmail.com>
2016-03-17 22:05:59 -0300

    Relacionamento com Marcas

--- app/Models/Produto.php
+++ app/Models/Produto.php
@@ -5,7 +5,9 @@ namespace Shoppvel\Models;
 use Illuminate\Database\Eloquent\Model;
 
 class Produto extends Model {
-
+    public function marca() {
+        return $this->belongsTo(Marca::class);
+    }
     
     public function scopeDestacado($query) {
         return $query->where('destacado', TRUE);
