1eb16549867931d6dfbd88b636f6c54fdcc5ef67
Ademir Mazer Jr <ademir.mazer.jr@gmail.com>
2016-03-22 22:33:00 -0300

    Melhorias no layout dos detalhes de produtos

--- resources/views/frente/produto-detalhes.blade.php
+++ resources/views/frente/produto-detalhes.blade.php
@@ -6,17 +6,26 @@
         {{$produto->nome}}
     </h2>
 </div>
-<div class="col-sm-6 col-md-4">
-    <div class="thumbnail">
-        <img src="{{route('imagem.file',$produto->imagem_nome)}}" >
-        <div class="caption">
+<div class="row">
+    <div class="col-sm-6">
+        <div class="thumbnail">
+            <img src="{{route('imagem.file',$produto->imagem_nome)}}" alt="{{$produto->imagem_nome}}">
+            <h4 class="text-muted">&copy; {{$produto->marca->nome}}</h4>
+            <h5 class="text-muted">categoria >> {{$produto->categoria->nome}}</h5>
         </div>
     </div>
+    <div class="col-sm-3">
+        <h2 class="text-center text-info"> R$ {{number_format($produto->preco_venda, 2, ',', '.')}}</h2>
+    </div>
+    <div class="col-sm-3">
+        <h3>Pagamento</h3>
+    </div>
 </div>
-<div class="col-md-4 col-sm-6">
-    {{$produto->preco_venda}}
-</div>
-<div class="col-md-4 col-sm-6">
-    {{$produto->descricao}}
+<div class="row">
+    <div class="col-sm-12">
+        <h3 class="page-header">Descrição detalhada</h3>
+        {{$produto->descricao}}
+    </div>
+
 </div>
 @stop
