940c8473dc675c9b366033ffac48836e02d04884
Ademir Mazer Jr <ademir.mazer.jr@gmail.com>
2016-03-23 22:03:19 -0300

    Rotas no middleware web para gerar o csrf token, por causa do campo de busca que aparece em todas as rotas

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,45 +1,48 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Routes File
+  |--------------------------------------------------------------------------
+  |
+  | Here is where you will register all of the routes in an application.
+  | It's a breeze. Simply tell Laravel the URIs it should respond to
+  | and give it the controller to call when that URI is requested.
+  |
+ */
 
-Route::get('/', 'FrenteLojaController@getIndex');
-Route::get('sobre', [
-    'as' => 'sobre',
-    'uses' => 'FrenteLojaController@getSobre'
-]);
-Route::get('categoria/{id?}', [
-    'as' => 'categoria.listar',
-    'uses' => 'CategoriaController@getCategoria'
-]);
-Route::get('produto/{id}', [
-    'as' => 'produto.detalhes',
-    'uses' => 'ProdutoController@getProdutoDetalhes'
-]);
-Route::get('imagem/arquivo/{nome}', [
-    'as' => 'imagem.file',
-    'uses' => 'ImagemController@getImagemFile'
-]);
 
 /*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| This route group applies the "web" middleware group to every route
-| it contains. The "web" middleware group is defined in your HTTP
-| kernel and includes session state, CSRF protection, and more.
-|
-*/
+  |--------------------------------------------------------------------------
+  | Application Routes
+  |--------------------------------------------------------------------------
+  |
+  | This route group applies the "web" middleware group to every route
+  | it contains. The "web" middleware group is defined in your HTTP
+  | kernel and includes session state, CSRF protection, and more.
+  |
+ */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+    Route::get('/', 'FrenteLojaController@getIndex');
+    Route::get('sobre', [
+        'as' => 'sobre',
+        'uses' => 'FrenteLojaController@getSobre'
+    ]);
+    Route::get('categoria/{id?}', [
+        'as' => 'categoria.listar',
+        'uses' => 'CategoriaController@getCategoria'
+    ]);
+    Route::get('produto/{id}', [
+        'as' => 'produto.detalhes',
+        'uses' => 'ProdutoController@getProdutoDetalhes'
+    ]);
+    Route::get('imagem/arquivo/{nome}', [
+        'as' => 'imagem.file',
+        'uses' => 'ImagemController@getImagemFile'
+    ]);
+    Route::any('produto/buscar', [
+        'as' => 'produto.buscar',
+        'uses' => 'ProdutoController@getBuscar'
+    ]);
 });
