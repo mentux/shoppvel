020096056900efa784f3705115560077416ce611
Ademir Mazer Jr <ademir.mazer.jr@gmail.com>
2016-05-03 20:18:49 -0300

    Modularização do cabeçalho

--- resources/views/layouts/frente-loja.blade.php
+++ resources/views/layouts/frente-loja.blade.php
@@ -35,43 +35,8 @@
 
         <div class="container">
 
-            <!-- The justified navigation menu is meant for single line per list item.
-                 Multiple lines will require custom code not provided by Bootstrap. -->
-            <div clas="row">
-                <h3 class="text-muted col-sm-2">Shoppvel</h3>
-                {!! Form::open(array('route' => 'produto.buscar', 'class'=>'navbar-form navbar-right')) !!} 
-                <div class="form-group">
-                    {!! Form::text('termo-pesquisa', null,['placeholder'=>'Pesquisar',
-                    'class'=>'form-control']) !!}
-                </div>
-                <button type="submit" class="btn btn-primary">
-                    <span class="glyphicon glyphicon-search" aria-hidden="true"></span>
-                </button>
-                {!! Form::close() !!}
-                <nav>
-                    <ul class="nav nav-justified">
-                        <li class="active"><a href="{{url('/')}}">Home</a></li>
-                        <li><a href="{{route('categoria.listar')}}">Categorias</a></li>
-                        <li><a href="{{route('carrinho.listar')}}">Carrinho</a></li>
-                        <li><a href="{{route('sobre')}}">Sobre</a></li>            
-
-                        @if (Auth::guest())
-                            <li><a href="{{ url('/login') }}">Login</a></li>
-                        @else
-                            <li class="small">
-                                {{ Auth::user()->name }}
-                            </li>
-                            <li>
-                                <a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                            </li>
-                        @endif
-
-                    </ul>
-                </nav>
-            </div>
-
-            <hr class="clearfix"/>
-
+            @include('layouts.frente-cabecalho')
+            
             <!-- Example row of columns -->
             <div class="row">
                 <div class="col-lg-2">
